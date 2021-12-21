9be3edc7759bfae62a52c6ae5caa0902af7651df
Jesse Ramon-Brown <jesseramonbrown@Jesses-MacBook-Pro.local>
2021-12-21 12:44:00 +0100

    add changes to fonts

--- src/components/Carrousel/referral-data.ts
+++ src/components/Carrousel/referral-data.ts
@@ -29,7 +29,7 @@ const data: ReferalData = [
     referral: {
       title: "Software for displaced populations in Iraq",
       referralText:
-        "Enverse Labs came on board to support us with the development of our software which helps <b>displaced populations</b> in Iraq access <b>life-saving information about the humanitarian response</b>. Among other things, they conducted an <b>extensive analysis of our existing software and codebase</b> to help identify pain points and much-needed upgrades. The team was professional, responsive, and very knowledgeable and <b>we would happily work with them again.</b>",
+        "Enverse Labs came on board to support us with the development of our software which helps displaced populations in Iraq access life-saving information about the humanitarian response. Among other things, they conducted an extensive analysis of our existing software and codebase to help identify pain points and much-needed upgrades. The team was professional, responsive, and very knowledgeable and we would happily work with them again.",
     },
   },
   {
@@ -45,7 +45,7 @@ const data: ReferalData = [
     referral: {
       title: "A new face for the leading social impact app",
       referralText:
-        "Enverse are talented professionals who really care about <b>social and environmental impact</b>. They are also fun to work with! If you are working on a social/environmental project, they will be great partners because they <b>share your mission.</b>",
+        "Enverse are talented professionals who really care about social and environmental impact. They are also fun to work with! If you are working on a social/environmental project, they will be great partners because they share your mission.",
     },
     project: {
       appLogoUrl: "/assets/image/socious-app.png",
@@ -65,7 +65,7 @@ const data: ReferalData = [
     referral: {
       title: "Dévelopement de Dérive",
       referralText:
-        "Enverse are helping us build the next version of <b>Dérive</b>. Thanks to their skills in development, <b>eco-conception and project management<b>, they allow us to continue developing a lightweight and pertinent application. Their strength of proposal, their listening skills, their rigor and their good mood make them ideal partners to develop our projects in confidence and in good intelligence.",
+        "Enverse are helping us build the next version of Dérive. Thanks to their skills in development, eco-conception and project management, they allow us to continue developing a lightweight and pertinent application. Their strength of proposal, their listening skills, their rigor and their good mood make them ideal partners to develop our projects in confidence and in good intelligence.",
     },
     project: {
       projectUrl: "https://derive.today",
