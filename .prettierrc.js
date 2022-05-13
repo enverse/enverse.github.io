dd20b7f78381b080e8333cb02d493729cbbbd34b
Chris <chrislhamelin@gmail.com>
2022-05-13 18:34:06 +0200

    add blog post init

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  parser: 'typescript',
+  trailingComma: 'none',
+  singleQuote: true,
+  // plugins: ['prettier-plugin-astro'],
+  // overrides: [
+  //   {
+  //     files: '*.astro',
+  //     options: { parser: 'astro' }
+  //   }
+  // ]
+};
